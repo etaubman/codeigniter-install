3accf07d3cebbe802b6d1edf04d4ec885b074c56
Ethan Taubman <etaubman@gmail.com>
2013-04-13 21:51:28 -0400

    Added base controller

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller {
+
+	/**
+	 * This controller is used for functions that need to happen either across multiple
+	 * controllers, or before all controllers.
+	 */
+	public function __construct ()
+	{
+		parent::__construct();
+	}
+}
+
+/* End of file MY_Controller.php */
+/* Location: ./application/controllers/MY_Controller.php */
